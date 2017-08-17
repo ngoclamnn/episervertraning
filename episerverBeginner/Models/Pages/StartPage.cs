a7bb1520656e9ba93a97956d63ad39e4683bfbcb
Lam Ngoc Nguyen <lam.nguyen2@niteco.se>
2017-08-17 13:17:53 +0700

    Add project files.

---
+++ episerverBeginner/Models/Pages/StartPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace episerverBeginner.Models.Pages
+{
+    [ContentType(DisplayName = "StartPage", GUID = "6a4f81d7-eb6e-4f4a-9ea5-5a54104b6568", Description = "")]
+    public class StartPage : PageData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
